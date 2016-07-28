482b90ad68f957486cb964bc6b6d789478e49d38
RichardK <uberviolence@gmail.com>
2016-07-28 11:50:41 +0300

    Json

--- app/src/main/java/fooddelivery/fooddelivery/MainActivity.java
+++ app/src/main/java/fooddelivery/fooddelivery/MainActivity.java
@@ -1,25 +1,159 @@
 package fooddelivery.fooddelivery;
 
+import android.app.Activity;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-
-    private Button btnUpdate;
-
+    EditText etResponse;
+    TextView tvIsConnected;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btnUpdate = (Button)findViewById(R.id.btnUpdate);
+        // get reference to the views
+        etResponse = (EditText) findViewById(R.id.etResponse);
+        tvIsConnected = (TextView) findViewById(R.id.tvIsConnected);
+
+        // check if you are connected or not
+        if(isConnected()){
+            tvIsConnected.setBackgroundColor(0xFF00CC00);
+            tvIsConnected.setText("You are conncted");
+        }
+        else{
+            tvIsConnected.setText("You are NOT conncted");
+        }
+
+        // call AsynTask to perform network operation on separate thread
+        new CategoriesAsyncTask().execute("http://www.mocky.io/v2/5799a988100000e2199e8316");
+
     }
 
-    public void btnUpdateClicked(View view) {
-        new DBJSONFetchTask().execute();
+    public static String GET(String url){
+        InputStream inputStream = null;
+        String result = "";
+        try {
+
+            // create HttpClient
+            HttpClient httpclient = new DefaultHttpClient();
+
+            // make GET request to the given URL
+            HttpResponse httpResponse = httpclient.execute(new HttpGet(url));
+
+            // receive response as inputStream
+            inputStream = httpResponse.getEntity().getContent();
+
+            // convert inputstream to string
+            if(inputStream != null)
+                result = convertInputStreamToString(inputStream);
+            else
+                result = "Did not work!";
+
+        } catch (Exception e) {
+            Log.d("InputStream", e.getLocalizedMessage());
+        }
+
+        return result;
+    }
+
+    private static String convertInputStreamToString(InputStream inputStream) throws IOException{
+        BufferedReader bufferedReader = new BufferedReader( new InputStreamReader(inputStream));
+        String line = "";
+        String result = "";
+        while((line = bufferedReader.readLine()) != null)
+            result += line;
+
+        inputStream.close();
+        return result;
+
+    }
+
+    public boolean isConnected(){
+        ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Activity.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+        if (networkInfo != null && networkInfo.isConnected())
+            return true;
+        else
+            return false;
+    }
+
+    private class RestaurantsAsyncTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... urls) {
+            return GET(urls[0]);
+        }
+
+        // onPostExecute displays the results of the AsyncTask.
+        @Override
+        protected void onPostExecute(String result) {
+            Toast.makeText(getBaseContext(), "Received!", Toast.LENGTH_LONG).show();
+            JSONArray jsonArray = null;
+            try {
+                jsonArray = new JSONArray(result);
+                JSONObject json;
+                for (int i = 0; i < jsonArray.length(); i++) {
+                    json = jsonArray.getJSONObject(i);
+                    etResponse.setText(etResponse.getText() + "\n" + json.getString("name"));
+
+                }
+
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+
+    private class CategoriesAsyncTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... urls) {
+            return GET(urls[0]);
+        }
+
+        // onPostExecute displays the results of the AsyncTask.
+        @Override
+        protected void onPostExecute(String result) {
+            Toast.makeText(getBaseContext(), "Received!", Toast.LENGTH_LONG).show();
+            JSONArray jsonArray = null;
+            try {
+                jsonArray = new JSONArray(result);
+                JSONObject json;
+                for (int i = 0; i < jsonArray.length(); i++) {
+                    json = jsonArray.getJSONObject(i);
+                    etResponse.setText(etResponse.getText() + "\n" + json.getString("name"));
+
+                }
+
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+        }
     }
 }
---
+++ app/src/main/java/fooddelivery/fooddelivery/Restaurant.java
@@ -0,0 +1,9 @@
+package fooddelivery.fooddelivery;
+
+public class Restaurant {
+
+    private long id;
+    private String name;
+
+
+}
